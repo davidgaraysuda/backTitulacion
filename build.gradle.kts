711af01a34d02cf88be1f8794cf5cdf4860eefc8
DGarayP <david10559@hotmail.com>
2023-08-13 11:07:19 -0500

    Main class 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 springBoot {
-	mainClass.set("ec.edu.sudamericano.practicasys")
+	mainClass.set("ec.edu.sudamericano.practicasys.PracticasysApplicationKt")
 }
 
 group = "ec.edu.sudamericano"
